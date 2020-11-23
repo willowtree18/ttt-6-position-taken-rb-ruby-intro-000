b678425af6e571aad99a28d07d20cf89c0e40a9f
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 01:13:55 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,11 +3,9 @@ def position_taken?(board, index)
 
 if board[index] == " " or "" or (nil)
   return (false)
-end
 
-if board[index] == "X" or "O"
+elsif board[index] == "X" or "O"
   return (true)
 end
-
 
 end
